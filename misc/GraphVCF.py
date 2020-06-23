70f32416a756507264dda6b6bd5d799a20c8f17b
Andrea Talenti <andrea.talenti@ed.ac.uk>
2020-06-23 08:34:10 +0100

    Generate graph VCF even in absence of genotypes

--- misc/GraphVCF.py
+++ misc/GraphVCF.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         elif '##' not in line and '#' in line:
             outfile.write(line)
             ninds = len(line.strip().split()) - 9
-        else:
+        elif '##' not in line and '#' not in line and len(line.split()) >= 10:
             line = line.strip().split()
             if line[2] != '.': line[2] = '.'
             if line[5] == '.': line[5] = '99'
@@ -72,6 +72,51 @@ if __name__ == "__main__":
 
             line[7] = ';'.join(['{}={}'.format(f, newNfo[f].lower()) for f in ['AC', 'LEN', 'NA', 'NS', 'TYPE']])
             outfile.write('{}\n'.format('\t'.join(line)))
+        else:
+            line = line.strip().split()
+            line += ["", ""]
+            if line[2] != '.': line[2] = '.'
+            if line[5] == '.': line[5] = '99'
+            if line[6] != '.': line[6] = '.'
+            
+            info = line[7].split(';')
+            
+            NS="0"
+            AC="0"
+            FORMAT="GT"
+
+            for f in line[7].split(";"):
+                if "NS=" in f:
+                    NS=f.split("=")[1]
+                if "AC" in f:
+                    AC=f.split("=")[1]
+
+            newNfo = {'NA': str(len(line[4].split(','))), 
+                        'NS': NS,
+                        'LEN': '',
+                        'TYPE': '',
+                        'AC': AC
+                    }
+            for f in info:
+                if 'SVLEN' in f: newNfo['LEN'] = f.split('=')[1]
+                if 'SVTYPE' in f: newNfo['TYPE'] = f.split('=')[1]
+
+            if newNfo['LEN'] == '' and len(line[3]) == len(line[4]) and len(line[3]) == 1:
+                newNfo['LEN'] = str(len(line[3]))
+                newNfo['TYPE'] = 'snp'
+            elif newNfo['LEN'] == '' and len(line[3]) == len(line[4]) and len(line[3]) > 1:
+                newNfo['LEN'] = str(len(line[3]))
+                newNfo['TYPE'] = 'mnp'
+            elif newNfo['LEN'] == '' and len(line[3]) != len(line[4]) and len(line[3]) > len(line[4]):
+                newNfo['LEN'] = str(abs(len(line[3]) - len(line[4])))
+                newNfo['TYPE'] = 'del'
+            elif newNfo['LEN'] == '' and len(line[3]) != len(line[4]) and len(line[3]) < len(line[4]):
+                newNfo['LEN'] = str(abs(len(line[3]) - len(line[4])))
+                newNfo['TYPE'] = 'ins'
+
+            line[7] = ';'.join(['{}={}'.format(f, newNfo[f].lower()) for f in ['AC', 'LEN', 'NA', 'NS', 'TYPE']])
+            outfile.write('{}\n'.format('\t'.join(line)))
+
     sys.stderr.write('All done.\n\n')
 
     
