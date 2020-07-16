a758137c09cce74024096631c5377ca9629cf6fb
Andrea Talenti <andrea.talenti@ed.ac.uk>
2020-07-16 11:26:12 +0100

    Filter out regions of high repetitiveness

--- ATAC-seq/01-FILTER_BLANK.sh
+++ ATAC-seq/01-FILTER_BLANK.sh
@@ -3,24 +3,27 @@
 
 for w in HEREFORD ANGUS NDAMA ANKOLE BRAHMAN PANGENOME; do
 	genome_len=`grep -w $w GENOME_SIZES.txt | awk '{print $2}'`;
+	genome_name=`python -c "import sys; print(sys.argv[1].lower())" $w `
 
-	if [ ! -e ${w}/NONHEREFORD/ ]; then mkdir ${w}/NONHEREFORD/; fi
+	if [ ! -e ${w}/BLANKPEAKS/ ]; then mkdir ${w}/BLANKPEAKS/; fi
 
-	for i in HF3457_nucfree_ATAC; do
-		if [ ! -e ${w}/NONHEREFORD/${i} ]; then
-	                mkdir ${w}/NONHEREFORD/${i};
+	for blank in HF3457_nucfree_ATAC; do
+		if [ ! -e ${w}/BLANKPEAKS/${blank} ]; then
+	                mkdir ${w}/BLANKPEAKS/${blank};
 	        fi;
-	        awk 'BEGIN{OFS="\t"};{print $1,$2,$3,$4"#"$9}' ${w}/GENRICH/${i}/*.narrowPeak > ${w}/NONHEREFORD/${i}/NONHEREFORD.bed
+	        awk 'BEGIN{OFS="\t"};{print $1,$2,$3,$4"#"$9}' ${w}/GENRICH/${blank}/*.narrowPeak > ${w}/BLANKPEAKS/${blank}/BLANKPEAKS.bed
 		echo "Blank ready."
 	done
 
 	for i in HF3457_Bcell_ATAC ND230_Bcell_ATAC Nelore1_Bcell_ATAC; do 
-		if [ ! -e ${w}/NONHEREFORD/${i} ]; then 
-			mkdir ${w}/NONHEREFORD/${i}; 
-		fi; 
-		bedtools subtract -A -f 0.50 -a ${w}/GENRICH/${i}/*.narrowPeak -b ${w}/NONHEREFORD/HF3457_nucfree_ATAC/NONHEREFORD.bed | sort | uniq > ${w}/GENRICH/${i}/${i}.noblank.narrowPeak
+		bedtools subtract -A -f 0.50 -a ${w}/GENRICH/${i}/${i}.narrowPeak -b ${w}/BLANKPEAKS/${blank}/BLANKPEAKS.bed | sort | uniq > ${w}/GENRICH/${i}/${i}.noblank.narrowPeak
+		bedtools subtract -A -f 0.50 -a ${w}/GENRICH/${i}/${i}.noblank.narrowPeak -b ./SOFTMASKED/${genome_name}.softmasked.bed | sort | uniq > ${w}/GENRICH/${i}/${i}.noblank.lowrep.narrowPeak
+		Rscript SCRIPTS/QSCORES.R ${w}/GENRICH/${i}/${i}.noblank.lowrep.narrowPeak $genome_len
 		Rscript SCRIPTS/QSCORES.R ${w}/GENRICH/${i}/${i}.noblank.narrowPeak $genome_len
 		Rscript SCRIPTS/QSCORES.R ${w}/GENRICH/${i}/${i}.narrowPeak $genome_len
+		echo "Initial peaks                 : "`wc -l ${w}/GENRICH/${i}/${i}.narrowPeak`
+		echo "No-blank peaks                : "`wc -l ${w}/GENRICH/${i}/${i}.noblank.narrowPeak`
+		echo "Low repetitive elements peaks : "`wc -l ${w}/GENRICH/${i}/${i}.noblank.lowrep.narrowPeak`
 	done
 	echo "Done ${w}"
 done
--- ATAC-seq/02-CROSSREFERENCE.sh
+++ ATAC-seq/02-CROSSREFERENCE.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CPG=$1
-
+cd PANGENOME
 if [ ! -e ./CROSSREF ]; then mkdir ./CROSSREF/; fi
 
 for i in HF3457_Bcell_ATAC ND230_Bcell_ATAC Nelore1_Bcell_ATAC; do
@@ -9,30 +9,31 @@ for i in HF3457_Bcell_ATAC ND230_Bcell_ATAC Nelore1_Bcell_ATAC; do
                 mkdir ./CROSSREF/${i};
         fi;
 
-        python SCRIPTS/ExtractBed.py GENRICH/${i}/*.noblank.withQ.narrowPeak CROSSREF/${i}/TOCROSSREF.noblank.withQ
-        python SCRIPTS/SplitByGenome.py CROSSREF/${i}/TOCROSSREF.noblank.withQ.narrowRegion.bed CROSSREF/${i}/narrow.noblank
+        python SCRIPTS/ExtractBed.py GENRICH/${i}/*.noblank.lowrep.withQ.narrowPeak CROSSREF/${i}/TOCROSSREF.noblank.lowrep.withQ
+        python SCRIPTS/SplitByGenome.py CROSSREF/${i}/TOCROSSREF.noblank.lowrep.withQ.narrowRegion.bed CROSSREF/${i}/narrow.noblank.lowrep
         echo "Validanting..."
         for w in hereford angus ankole brahman ndama; do
                 upname=`python -c "import sys; print(sys.argv[1].upper())" ${w}`
-                bedtools intersect -a CROSSREF/${i}/narrow.noblank.${w}.bed -b ../${upname}/GENRICH/${i}/${i}.bed -f 0.9 -wa |\
-                        sort | uniq > CROSSREF/${i}/narrow.noblank.${w}.crossreferenced.bed
-                bedtools intersect -a CROSSREF/${i}/narrow.noblank.${w}.bed -b ../${upname}/GENRICH/${i}/${i}.bed -f 0.9 -wa -wb |\
-                        sort | uniq > CROSSREF/${i}/narrow.noblank.${w}.crossreferenced.large.bed
+                bedtools intersect -a CROSSREF/${i}/narrow.noblank.lowrep.${w}.bed -b ../${upname}/GENRICH/${i}/${i}.bed -f 0.9 -wa |\
+                        sort | uniq > CROSSREF/${i}/narrow.noblank.lowrep.${w}.crossreferenced.bed
+                bedtools intersect -a CROSSREF/${i}/narrow.noblank.lowrep.${w}.bed -b ../${upname}/GENRICH/${i}/${i}.bed -f 0.9 -wa -wb |\
+                        sort | uniq > CROSSREF/${i}/narrow.noblank.lowrep.${w}.crossreferenced.large.bed
                 echo "In CpG..."
-                bedtools intersect -a CROSSREF/${i}/narrow.noblank.${w}.crossreferenced.bed -b $CPG/CPG.${w}.bed -wa -wb > CROSSREF/${i}/narrow.noblank.${w}.crossreferenced.inCpG.bed
+                bedtools intersect -a CROSSREF/${i}/narrow.noblank.lowrep.${w}.crossreferenced.bed -b $CPG/CPG.${w}.bed -wa -wb > CROSSREF/${i}/narrow.noblank.lowrep.${w}.crossreferenced.inCpG.bed
         done
-        cat CROSSREF/${i}/narrow.noblank.*.crossreferenced.bed > CROSSREF/${i}/narrow.noblank.crossreferenced.bed
-        cat CROSSREF/${i}/narrow.noblank.*.crossreferenced.large.bed > CROSSREF/${i}/narrow.noblank.crossreferenced.large.bed
-        cat CROSSREF/${i}/narrow.noblank.*.crossreferenced.inCpG.bed > CROSSREF/${i}/narrow.noblank.crossreferenced.inCpG.bed
+        cat CROSSREF/${i}/narrow.noblank.lowrep.*.crossreferenced.bed > CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.bed
+        cat CROSSREF/${i}/narrow.noblank.lowrep.*.crossreferenced.large.bed > CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.large.bed
+        cat CROSSREF/${i}/narrow.noblank.lowrep.*.crossreferenced.inCpG.bed > CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.inCpG.bed
         # Generate narrowPeak crossreferenced file
         echo "Prepare narrowPeak files..."
-        python SCRIPTS/getNarrowPeaks.py GENRICH/${i}/*.noblank.withQ.narrowPeak CROSSREF/${i}/narrow.noblank.crossreferenced.bed > CROSSREF/${i}/narrow.noblank.crossreferenced.narrowPeak
-        python SCRIPTS/getNarrowPeaks.py GENRICH/${i}/*.noblank.withQ.narrowPeak CROSSREF/${i}/narrow.noblank.crossreferenced.InCpG.bed > CROSSREF/${i}/narrow.noblank.crossreferenced.InCpG.narrowPeak
-        echo "Cross-references  :      "`wc -l CROSSREF/${i}/narrow.noblank.crossreferenced.narrowPeak`
-        echo "In CpG            :      "`wc -l CROSSREF/${i}/narrow.noblank.crossreferenced.InCpG.narrowPeak`
+        python SCRIPTS/getNarrowPeaks.py GENRICH/${i}/*.noblank.withQ.narrowPeak CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.bed > CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.narrowPeak
+        python SCRIPTS/getNarrowPeaks.py GENRICH/${i}/*.noblank.withQ.narrowPeak CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.InCpG.bed > CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.InCpG.narrowPeak
+        echo "Cross-references  :      "`wc -l CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.narrowPeak`
+        echo "In CpG            :      "`wc -l CROSSREF/${i}/narrow.noblank.lowrep.crossreferenced.InCpG.narrowPeak`
         # Cleaning
         echo "Cleaning..."
         for w in hereford angus ankole brahman ndama; do rm CROSSREF/${i}/*.${w}.*; done
         echo "Done $i"
         echo
 done
+cd ../
