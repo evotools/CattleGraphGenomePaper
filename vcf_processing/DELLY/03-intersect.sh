6ee69aa9beb75273a8ea6f0eb5cf1beb2f522c87
Andrea Talenti <andrea.talenti@ed.ac.uk>
2021-05-20 12:38:39 +0100

    Improve intersection script for Delly results

--- vcf_processing/DELLY/03-intersect.sh
+++ vcf_processing/DELLY/03-intersect.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 for i in ANGUS NDAMA SAHIWAL; do 
-    bedtools intersect -a ../MERGED/$i/$i.SV500.vcf -b ../regions.bed -wa -u -header > ../MERGED/$i/$i.SV500.isecBNOM.vcf 
+    awk '$1~"#"{print};$1!~"#" && $0!~"SVTYPE=TRA"{print}' ../MERGED/$i/$i.SV500.vcf | \
+        bedtools intersect -a - -b ../regions.bed -wa -u -header  > ../MERGED/$i/$i.SV500.noTRA.isecBNOM.vcf 
 done
-
-bedtools intersect -a ../MERGED/ALL/delly.merged.min500bp.1suppcall.100bp_breakpoint.vcf -b ../regions.bed -wa -u -header > ../MERGED/ALL/delly.merged.min500bp.1suppcall.100bp_breakpoint.isecBNOM.vcf
+awk '$1~"#"{print};$1!~"#" && $0!~"SVTYPE=TRA"{print}' ../MERGED/ALL/delly.merged.min500bp.1suppcall.100bp_breakpoint.vcf |
+    bedtools intersect -a - -b ../regions.bed -wa -u -header > ../MERGED/ALL/delly.merged.min500bp.1suppcall.100bp_breakpoint.noTRA.isecBNOM.vcf
