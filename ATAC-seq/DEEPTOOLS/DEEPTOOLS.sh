1c0cccc9af03d69ea7e605301ebe332dba0b8f45
Andrea Talenti <andrea.talenti@ed.ac.uk>
2021-05-27 18:48:07 +0100

    Fix bug in DEEPTOOLS script

--- ATAC-seq/DEEPTOOLS/DEEPTOOLS.sh
+++ ATAC-seq/DEEPTOOLS/DEEPTOOLS.sh
@@ -15,15 +15,15 @@ source activate deeptools
 
 echo "Compute general matrix and plot"
 computeMatrix scale-regions -S DEEPTOOLS/MULTI/HF3457_Bcell_ATAC/HF3457_Bcell_ATAC.bw DEEPTOOLS/MULTI/ND230_Bcell_ATAC/ND230_Bcell_ATAC.bw DEEPTOOLS/MULTI/Nelore2_Bcell_ATAC/Nelore2_Bcell_ATAC.bw -R FEATURES/Bos_taurus.ARS-UCD1.2.103.chr.gtf --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o ./DEEPTOOLS/MULTI/matrix.genes.multi.mat.gz --missingDataAsZero -p 4 --skipZeros --samplesLabel "Holstein 1" "N'Dama 2" "Nelore 1"
-plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.genes.multi.mat.gz -out ./DEEPTOOLS/MULTI/multi_GenesHeatmap.png
+plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.genes.multi.mat.gz --averageTypeSummaryPlot median -out ./DEEPTOOLS/MULTI/multi_GenesHeatmap.png
 
 echo "Compute novel contigs matrix and plot"
 computeMatrix scale-regions -S DEEPTOOLS/MULTI/HF3457_Bcell_ATAC/HF3457_Bcell_ATAC.bw DEEPTOOLS/MULTI/ND230_Bcell_ATAC/ND230_Bcell_ATAC.bw DEEPTOOLS/MULTI/Nelore2_Bcell_ATAC/Nelore2_Bcell_ATAC.bw -R FEATURES/predicted.abInitio.bed --beforeRegionStartLength 3000 --regionBodyLength 5000 --afterRegionStartLength 3000 -o ./DEEPTOOLS/MULTI/matrix.augustus.multi.mat.gz --missingDataAsZero -p 4 --skipZeros --samplesLabel "Holstein 1" "N'Dama 2" "Nelore 1"
-plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.augustus.multi.mat.gz -out ./DEEPTOOLS/MULTI/multi_augustusGenesHeatmap.png
+plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.augustus.multi.mat.gz --averageTypeSummaryPlot median -out ./DEEPTOOLS/MULTI/multi_augustusGenesHeatmap.png
 
 # repeat reference point
 computeMatrix reference-point -S DEEPTOOLS/MULTI/HF3457_Bcell_ATAC/HF3457_Bcell_ATAC.bw DEEPTOOLS/MULTI/ND230_Bcell_ATAC/ND230_Bcell_ATAC.bw DEEPTOOLS/MULTI/Nelore2_Bcell_ATAC/Nelore2_Bcell_ATAC.bw -R FEATURES/Bos_taurus.ARS-UCD1.2.103.chr.gtf -a 3000 -b 3000 -o ./DEEPTOOLS/MULTI/matrix.genes.multi.refpoint.mat.gz --missingDataAsZero -p $NSLOTS --skipZeros --samplesLabel "Holstein 1" "N'Dama 2" "Nelore 1"
-plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.genes.multi.refpoint.mat.gz -out ./DEEPTOOLS/MULTI/multi_GenesHeatmap_refpoint.png
+plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.genes.multi.refpoint.mat.gz --averageTypeSummaryPlot median -out ./DEEPTOOLS/MULTI/multi_GenesHeatmap_refpoint.png
 
 computeMatrix reference-point -S DEEPTOOLS/MULTI/HF3457_Bcell_ATAC/HF3457_Bcell_ATAC.bw DEEPTOOLS/MULTI/ND230_Bcell_ATAC/ND230_Bcell_ATAC.bw DEEPTOOLS/MULTI/Nelore2_Bcell_ATAC/Nelore2_Bcell_ATAC.bw -R FEATURES/predicted.abInitio.bed -a 3000 -b 3000 -o ./DEEPTOOLS/MULTI/matrix.augustus.multi.refpoint.mat.gz --missingDataAsZero -p $NSLOTS --skipZeros --samplesLabel "Holstein 1" "N'Dama 2" "Nelore 1"
-plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.augustus.multi.refpoint.mat.gz -out ./DEEPTOOLS/MULTI/multi_augustusGenesHeatmap_refpoint.png
+plotHeatmap -m ./DEEPTOOLS/MULTI/matrix.augustus.multi.refpoint.mat.gz --averageTypeSummaryPlot median -out ./DEEPTOOLS/MULTI/multi_augustusGenesHeatmap_refpoint.png
